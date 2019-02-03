b64a831d35337bffaddfa5f1155714893741d01c
kyujyokei <kyujyokei@gmail.com>
2019-02-02 16:41:53 -0800

    Basic webcam showing, added contraints for rear camera

--- src/App.js
+++ src/App.js
@@ -1,46 +1,52 @@
 import React, { Component } from 'react';
-import Webcam from './Webcam';
 import * as ml5 from 'ml5';
 import './App.css';
 
 
 class App extends Component {
-  video = <video id="video" width="640" height="480" autoplay></video>;
+  video = React.createRef();
 
   state = {
-    result : null
+    result :null
   }
 
   loop = (classifier) => {
     classifier.predict()
       .then(results => {
         this.setState({result: results[0].className});
-        // probability.innerText = results[0].probability.toFixed(4);
         this.loop(classifier) // Call again to create a loop
       })
   }
-  
 
   componentDidMount(){
-    ml5.imageClassifier('MobileNet', this.video)
-      .then(classifier => this.loop(classifier))
+    // ml5.imageClassifier('MobileNet', this.video.current)
+    //   .then(classifier => this.loop(classifier))
 
   }
   render() {
 
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then((stream) => {
-        this.video.srcObject = stream;
-        this.video.play();
-      })
+    const constraints = {
+        advanced: [{
+            facingMode: "environment"
+        }]
+    };
 
-    
+    navigator.mediaDevices.getUserMedia({ video: constraints })
+      .then((stream) => {
+        if ( this.video.current ) {
+          this.video.current.srcObject = stream;
+          this.video.current.play();
+        }
+      })
   
     return (
       <div className="App">
-        {this.video}
-        
-      
+        <video ref={ this.video } 
+             id="video" 
+             width="640" 
+             height="480" 
+             autoPlay
+      />
       </div>
     );
   }
