a90d34c6656a846ef776e1323c4ff792a73634d8
kyujyokei <kyujyokei@gmail.com>
2019-02-06 12:34:54 -0800

    Finally able to run properly without any p5

--- src/App.js
+++ src/App.js
@@ -15,40 +15,34 @@ class App extends Component {
     classifier.predict()
       .then(results => {
         this.setState({result: results[0].className});
+        // console.log(results[0].className);
         this.loop(classifier) // Call again to create a loop
       })
   }
 
   componentDidMount(){
-    // ml5.imageClassifier('MobileNet', this.video.current)
-    //   .then(classifier => this.loop(classifier))
+    navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+      if (this.video.current) {
+        this.video.current.srcObject = stream;
+        this.video.current.play();
+      }
+  
+      ml5.imageClassifier("MobileNet", this.video.current)
+         .then(classifier => this.loop(classifier));
+    });
 
   }
   render() {
-
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then((stream) => {
-        if ( this.video.current ) {
-          this.video.current.srcObject = stream;
-          this.video.current.play();
-        }
-      })
   
     return (
       <div className="App">
-        {/* <video ref={ this.video } 
+        <video ref={ this.video } 
              id="video" 
              width="640" 
              height="480" 
              autoPlay
-      /> */}
-        <Webcam
-          audio={false}
-          height={350}
-          ref={this.setRef}
-          screenshotFormat="image/jpeg"
-          width={350}
-        />
+      />
+      <p>{this.state.result}</p>
       </div>
     );
   }
