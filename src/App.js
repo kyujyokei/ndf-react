ee60f66f79e373120b08aa3f062d2f6c79f2618a
kyujyokei <kyujyokei@gmail.com>
2019-02-06 20:21:32 -0800

    Able to function normally with ant design framework

--- src/App.js
+++ src/App.js
@@ -1,34 +1,96 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import * as ml5 from 'ml5';
 import './App.css';
-import sketch from './sketch';
-import P5Wrapper from 'react-p5-wrapper';
-
-import {label} from './sketch';
-
-function getCoords(){
-  console.log(arguments);
-}
+import { Modal, Button, Spin } from 'antd';
 
 class App extends Component {
+  video = React.createRef();
+
+  state = {
+    result :null,
+    prob: null,
+    visible: false
+  }
+
+  loop = (classifier) => {
+    classifier.predict()
+      .then(results => {
+        this.setState({result: results[0].className, prob: results[0].probability});
+        // console.log(results);
+        this.loop(classifier) // Call again to create a loop
+      })
+  }
 
   componentDidMount(){
-
+    navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+      if (this.video.current) {
+        this.video.current.srcObject = stream;
+        this.video.current.play();
+      }
+  
+      ml5.imageClassifier("MobileNet", this.video.current)
+         .then(classifier => this.loop(classifier));
+    });
   }
-  render() {
-    return (
-      <div className="App">
 
-        <P5Wrapper sketch={sketch} getCoords={getCoords}/>
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  render() {
+
+    
+
+    return (
+      <div>
+
+        {this.state.result ? null : <Spin />}
+        {/* <Button type="primary" onClick={this.showModal}>
+          Open Modal
+        </Button>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal> */}
+        
+        <video ref={ this.video } 
+                  id="video" 
+                  width="640" 
+                  height="480" 
+                  autoPlay
+                  // className={classes.Video}
+                />
+        {/* <div className={classes.Contents}> */}
+        <div>
+          <p>{this.state.result ? this.state.result + ',' + this.state.prob : <Spin />}</p>
+        </div>
+        
       </div>
     );
   }
 }
 
-export default App;
 
-document.body.onkeyup = function(e){
-  if(e.keyCode == 32){
-      console.log(label);
-  }
-}
+export default App;
